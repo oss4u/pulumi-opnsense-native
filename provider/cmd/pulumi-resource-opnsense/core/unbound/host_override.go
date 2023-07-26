12732484d08143781532adfe147453cbcab16ab7
Marc Ende <me@e-beyond.de>
2023-07-26 13:13:36 +0200

    fix: Implemented Read

--- provider/cmd/pulumi-resource-opnsense/core/unbound/host_override.go
+++ provider/cmd/pulumi-resource-opnsense/core/unbound/host_override.go
@@ -104,45 +104,45 @@ func (h HostOverride) Diff(ctx p.Context, id string, old HostOverrideState, new 
 	}
 	diffs := map[string]p.PropertyDiff{}
 	if host.Host.Hostname != *new.Hostname {
-		ctx.Log(diag.Debug, fmt.Sprintf("Hostname differs: %s/%s", *old.Hostname, *new.Hostname))
+		ctx.Log(diag.Info, fmt.Sprintf("Hostname differs: %s/%s", host.Host.Hostname, *new.Hostname))
 		diffs["hostname"] = p.PropertyDiff{
 			Kind: p.Update,
 		}
 	}
 	if host.Host.Domain != *new.Domain {
-		ctx.Log(diag.Debug, fmt.Sprintf("Domain differs: %s/%s", *old.Domain, *new.Domain))
+		ctx.Log(diag.Info, fmt.Sprintf("Domain differs: %s/%s", host.Host.Domain, *new.Domain))
 		diffs["domain"] = p.PropertyDiff{
 			Kind: p.Update,
 		}
 	}
 	if host.Host.Description != *new.Description {
-		ctx.Log(diag.Debug, fmt.Sprintf("Description differs: %s/%s", *old.Description, *new.Description))
+		ctx.Log(diag.Info, fmt.Sprintf("Description differs: %s/%s", host.Host.Description, *new.Description))
 		diffs["description"] = p.PropertyDiff{
 			Kind: p.Update,
 		}
 	}
 	if host.Host.Enabled != *new.Enabled {
-		ctx.Log(diag.Debug, fmt.Sprintf("Enabled differs: %s/%s", *old.Enabled, *new.Enabled))
+		ctx.Log(diag.Info, fmt.Sprintf("Enabled differs: %s/%s", host.Host.Enabled, *new.Enabled))
 		diffs["enabled"] = p.PropertyDiff{
 			Kind: p.Update,
 		}
 	}
 	if host.Host.Rr == "A" {
-		if *old.Server != *new.Server {
-			ctx.Log(diag.Debug, fmt.Sprintf("Server differs: %s/%s", *old.Server, *new.Server))
+		if host.Host.Server != *new.Server {
+			ctx.Log(diag.Info, fmt.Sprintf("Server differs: %s/%s", host.Host.Server, *new.Server))
 			diffs["server"] = p.PropertyDiff{
 				Kind: p.Update,
 			}
 		}
 	} else if host.Host.Rr == "MX" {
 		if host.Host.Mx != *new.Mx {
-			ctx.Log(diag.Debug, fmt.Sprintf("Mx differs: %s/%s", *old.Mx, *new.Mx))
+			ctx.Log(diag.Info, fmt.Sprintf("Mx differs: %s/%s", host.Host.Mx, *new.Mx))
 			diffs["mx"] = p.PropertyDiff{
 				Kind: p.Update,
 			}
 		}
 		if host.Host.Mxprio != *new.MxPrio {
-			ctx.Log(diag.Debug, fmt.Sprintf("MxPrio differs: %s/%s", *old.MxPrio, *new.MxPrio))
+			ctx.Log(diag.Info, fmt.Sprintf("MxPrio differs: %s/%s", host.Host.Mxprio, *new.MxPrio))
 			diffs["mxprio"] = p.PropertyDiff{
 				Kind: p.Update,
 			}
